0a40791fa742122661124003b12a5a66d1182712
t-bittco <tasu.2331@gmail.com>
2019-12-08 15:35:11 +0900

    メッセージ機能実装
    1.form実装
    2.メッセージをメイン画面に反映
    3.グループ編集機能追加

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,11 +1,19 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: [:edit, :update]
   def index
+    
   end
-
   def new
     @group = Group.new
     @group.users << current_user
+  end
+
+  def update
+    if @group.update(group_params) 
+      redirect_to group_messages_path(params[:id]), notice: 'グループを更新しました'
+    else
+      render :edit
+    end
   end
 
   def create
@@ -17,24 +25,12 @@ class GroupsController < ApplicationController
     end
   end
 
-  def edit
-    if @group.update(group_params)
-      redirect_to root_path, notice: 'グループを更新しました'
-    else
-      render :edit
-    end
-  end
-  
-  def update
-    
-  end
-
   private
   def group_params
     params.require(:group).permit(:name, user_ids: [] )
   end
+
   def set_group
     @group = Group.find(params[:id])
   end
-
 end
