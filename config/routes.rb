e35e9d094415c986cf13a7033ac6af8316fcd310
t-bittco <tasu.2331@gmail.com>
2019-12-23 21:22:12 +0900

    veiwの修正
    メッセージを送るとビューが崩れるバグの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, except: [:edit, :update]
+  # devise_for :users
   root 'groups#index'
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
